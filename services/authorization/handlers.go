cdec3dd9ce339096ab16800df9246883d1cf0a25
vpachkov <slava.pach@gmail.com>
2021-10-02 09:41:10 +0300

    [servers] auth

--- services/authorization/handlers.go
+++ services/authorization/handlers.go
@@ -5,9 +5,15 @@ import (
 	"uchicollab/db"
 )
 
-func handleAuthorize(request authorizeRequest) (authorizeResponse, int) {
+func handleAuthorize(request authorizeRequest) (response authorizeResponse, status int) {
 	dbi := db.Get()
-	var session db.Session
-	dbi.First(&session)
-	return authorizeResponse{Session: session.ID}, http.StatusOK
+	var sessions []db.Session
+	dbi.Preload("User").Find(&sessions)
+	for _, session := range sessions {
+		if session.User.Login == "russcox" {
+			return authorizeResponse{Session: session.ID}, http.StatusOK
+		}
+	}
+	status = http.StatusOK
+	return
 }
